e9b68017d577ad5218345f07f50dd55a14cce470
fabian thomas stuck <stuck.fabian@gmail.com>
2024-06-13 14:48:38 +0200

    comment

--- appChat/chatter.go
+++ appChat/chatter.go
@@ -32,6 +32,7 @@ func (app *App) RemoveChatter(chatterId string) error {
 	if room != nil {
 		delete(room.chatters, chatterId)
 		if len(room.chatters) == 0 {
+			//chat room is deleted once all chatters leave. that is why messages only persist while chatters are in the room
 			delete(app.rooms, chatter.roomId)
 		}
 	}
