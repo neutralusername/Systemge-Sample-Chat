56ea73dbb798dc49d558e8cefd747ce980a564b6
fabian thomas stuck <stuck.fabian@gmail.com>
2024-06-13 14:34:29 +0200

    refactoring

--- main/main.go
+++ main/main.go
@@ -13,15 +13,15 @@ const WEBSOCKET_PORT = ":8443"
 const ERROR_LOG_FILE_PATH = "error.log"
 
 func main() {
+	//resolver and brokers are placed outside, because on startup they need to be started first.
+	//and if they are closed first on stop, the other modules will not be able to communicate with them.
+	//this demonstrates why multi modules are not always the best solution.
 	Module.NewResolverServerFromConfig("resolver.systemge", ERROR_LOG_FILE_PATH).Start()
 	Module.NewBrokerServerFromConfig("brokerChat.systemge", ERROR_LOG_FILE_PATH).Start()
 	Module.NewBrokerServerFromConfig("brokerWebsocket.systemge", ERROR_LOG_FILE_PATH).Start()
 
 	clientChat := Module.NewClient("clientApp", TOPICRESOLUTIONSERVER_ADDRESS, ERROR_LOG_FILE_PATH, appChat.New)
 	Module.StartCommandLineInterface(Module.NewMultiModule(
-		//resolver and brokers are placed outside, because on startup they need to be started first.
-		//and if they are closed first on stop, the other modules will not be able to communicate with them.
-		//this demonstrates why multi modules are not always the best solution.
 
 		//order is important in this multi module because websocket disconnects all clients when it stops and within the disconnect routine it communicates to the chat app that the client has disconnected.
 		//if the chat app is stopped before the websocket client, the chat app will not be able to communicate to the websocket client that the client has disconnected.
