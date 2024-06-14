04b479cf81fd59a4e2f00734eea73115623d8ee1
fabian thomas stuck <stuck.fabian@gmail.com>
2024-06-14 08:45:34 +0200

    fixed comment (mixed up chat and websocket app)

--- main/main.go
+++ main/main.go
@@ -24,8 +24,8 @@ func main() {
 	clientWebsocket := Module.NewWebsocketClient("clientWebsocket", TOPICRESOLUTIONSERVER_ADDRESS, ERROR_LOG_FILE_PATH, "/ws", WEBSOCKET_PORT, "", "", appWebsocket.New)
 	Module.StartCommandLineInterface(Module.NewMultiModule(
 		//order is important in this multi module because websocket app disconnects all clients when it stops and within onDisconnct() it communicates to the chat app that the client has disconnected.
-		//if the chat app is stopped before the websocket app, the chat app will not be able to communicate to the websocket app that the client has disconnected.
-		//which results in the websocket app having chatters that will never be removed.
+		//if the chat app is stopped before the websocket app, the websocket app will not be able to communicate to the chat app that the client has disconnected.
+		//which results in the chat app having chatters that will never be removed.
 		clientWebsocket,
 		clientChat,
 		Module.NewHTTPServerFromConfig("httpServe.systemge", ERROR_LOG_FILE_PATH),
