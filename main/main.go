b88e36900f2ba6a7614fc074203485361d79be3b
fabian thomas stuck <stuck.fabian@gmail.com>
2024-06-15 14:22:11 +0200

    refactoring

--- main/main.go
+++ main/main.go
@@ -16,9 +16,18 @@ func main() {
 	//and if they are stopped first on stop, which is inherent to multi-module behaviour, the other modules will not be able to communicate with them during their stop/disconnect routine.
 	//this demonstrates why multi modules are not always the best solution.
 	//the alternative is to start them either manually, like here in the main function, or to start them in separate terminal windows as separate processes/programs.
-	Module.NewResolverFromConfig("resolver.systemge", ERROR_LOG_FILE_PATH).Start()
-	Module.NewBrokerFromConfig("brokerChat.systemge", ERROR_LOG_FILE_PATH).Start()
-	Module.NewBrokerFromConfig("brokerWebsocket.systemge", ERROR_LOG_FILE_PATH).Start()
+	err := Module.NewResolverFromConfig("resolver.systemge", ERROR_LOG_FILE_PATH).Start()
+	if err != nil {
+		panic(err)
+	}
+	err = Module.NewBrokerFromConfig("brokerChat.systemge", ERROR_LOG_FILE_PATH).Start()
+	if err != nil {
+		panic(err)
+	}
+	err = Module.NewBrokerFromConfig("brokerWebsocket.systemge", ERROR_LOG_FILE_PATH).Start()
+	if err != nil {
+		panic(err)
+	}
 
 	clientChat := Module.NewClient("clientApp", TOPICRESOLUTIONSERVER_ADDRESS, ERROR_LOG_FILE_PATH, appChat.New, nil)
 	clientWebsocket := Module.NewWebsocketClient("clientWebsocket", TOPICRESOLUTIONSERVER_ADDRESS, ERROR_LOG_FILE_PATH, "/ws", WEBSOCKET_PORT, "", "", appWebsocket.New, nil)
