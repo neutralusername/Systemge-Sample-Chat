3d9fec96ab1e08a5023443cfac74a4e2353ce984
fabian thomas stuck <stuck.fabian@gmail.com>
2024-06-28 11:05:47 +0200

    comment

--- main/main.go
+++ main/main.go
@@ -16,7 +16,7 @@ func main() {
 	//resolver and brokers are placed outside, because on startup they need to be started first.
 	//and if they are stopped first on stop, which is inherent to multi-module behaviour, the other modules will not be able to communicate with them during their stop/disconnect routine.
 	//this demonstrates why multi modules are not always the best solution.
-	//the alternative is to start them either manually, like here in the main function, or to start them in separate terminal windows as separate processes/programs.
+	//the alternative is to start them either manually, like here in the main function, or to start them in separate terminal windows as separate processes/programs with their own command line interfaces.
 	err := Resolver.New(Config.ParseResolverConfigFromFile("resolver.systemge")).Start()
 	if err != nil {
 		panic(err)
