db7cc34aaa423708c599f0e83137c79b3585718f
fabian thomas stuck <stuck.fabian@gmail.com>
2024-06-28 10:34:08 +0200

    refactoring

--- main/main.go
+++ main/main.go
@@ -17,15 +17,15 @@ func main() {
 	//and if they are stopped first on stop, which is inherent to multi-module behaviour, the other modules will not be able to communicate with them during their stop/disconnect routine.
 	//this demonstrates why multi modules are not always the best solution.
 	//the alternative is to start them either manually, like here in the main function, or to start them in separate terminal windows as separate processes/programs.
-	err := Resolver.New(Module.ParseResolverConfigFromFile("resolver.systemge")).Start()
+	err := Resolver.New(Config.ParseResolverConfigFromFile("resolver.systemge")).Start()
 	if err != nil {
 		panic(err)
 	}
-	err = Broker.New(Module.ParseBrokerConfigFromFile("brokerChat.systemge")).Start()
+	err = Broker.New(Config.ParseBrokerConfigFromFile("brokerChat.systemge")).Start()
 	if err != nil {
 		panic(err)
 	}
-	err = Broker.New(Module.ParseBrokerConfigFromFile("brokerWebsocketHTTP.systemge")).Start()
+	err = Broker.New(Config.ParseBrokerConfigFromFile("brokerWebsocketHTTP.systemge")).Start()
 	if err != nil {
 		panic(err)
 	}
