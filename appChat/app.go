5ee121864bda423151f4abcab80412f2b49bfaf8
fabian thomas stuck <stuck.fabian@gmail.com>
2024-07-21 08:36:00 +0200

    implemented systemge changes

--- appChat/app.go
+++ appChat/app.go
@@ -28,27 +28,29 @@ func (app *App) GetCommandHandlers() map[string]Node.CommandHandler {
 	}
 }
 
-func (app *App) GetChatters(node *Node.Node, args []string) error {
+func (app *App) GetChatters(node *Node.Node, args []string) (string, error) {
 	app.mutex.Lock()
 	defer app.mutex.Unlock()
 	if len(args) != 1 {
-		return Error.New("Invalid arguments", nil)
+		return "", Error.New("Invalid arguments", nil)
 	}
 	room := app.rooms[args[0]]
 	if room == nil {
-		return Error.New("Room not found", nil)
+		return "", Error.New("Room not found", nil)
 	}
+	resultStr := ""
 	for _, chatter := range room.chatters {
-		println(chatter.id)
+		resultStr += chatter.id + ";"
 	}
-	return nil
+	return resultStr, nil
 }
 
-func (app *App) GetRooms(node *Node.Node, args []string) error {
+func (app *App) GetRooms(node *Node.Node, args []string) (string, error) {
 	app.mutex.Lock()
 	defer app.mutex.Unlock()
+	resultStr := ""
 	for roomId := range app.rooms {
-		println(roomId)
+		resultStr += roomId + ";"
 	}
-	return nil
+	return resultStr, nil
 }
